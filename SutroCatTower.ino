0f53d94a4639b8f16ad15452fb5981fb95e26057
Kyle Anderson <kyle@xkyle.com>
2021-01-25 17:16:33 -0800

    Working multilevel plasma

--- SutroCatTower.ino
+++ SutroCatTower.ino
@@ -18,6 +18,19 @@
 #define NUM_LEDS (NUM_PLATFORM_1_LEDS + NUM_PLATFORM_2_LEDS + NUM_PLATFORM_3_LEDS + NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 3
 #define NUM_LOWER_LEDS (NUM_PLATFORM_1_LEDS + NUM_PLATFORM_2_LEDS + NUM_PLATFORM_3_LEDS) * 3
 CRGB leds[NUM_LEDS];
+int platform_1_leds[NUM_PLATFORM_1_LEDS * 3];
+int platform_2_leds[NUM_PLATFORM_2_LEDS * 3];
+int platform_3_leds[NUM_PLATFORM_3_LEDS * 3];
+int platform_4_leds[NUM_PLATFORM_4_LEDS * 3];
+int spot_leds[NUM_SPOT_LEDS * 3];
+int antenna_leds[NUM_ANTENNA_LEDS * 3];
+
+CRGBPalette16 platform_1_currentPalette;
+CRGBPalette16 platform_1_targetPalette;
+CRGBPalette16 platform_2_currentPalette;
+CRGBPalette16 platform_2_targetPalette;
+CRGBPalette16 platform_3_currentPalette;
+CRGBPalette16 platform_3_targetPalette;
 
 void setup()
 {
@@ -33,88 +46,25 @@ void setup()
 
 void figureOutWhatToShow()
 {
-  int offset, segment_end;
+  Serial.println("Platform 1");
+  plasma(platform_1_leds, NUM_PLATFORM_1_LEDS * 3, platform_1_currentPalette, platform_1_targetPalette);
+  Serial.println("Platform 2");
+  plasma(platform_2_leds, NUM_PLATFORM_2_LEDS * 3, platform_2_currentPalette, platform_2_targetPalette);
+  Serial.println("Platform 3");
+  plasma(platform_3_leds, NUM_PLATFORM_3_LEDS * 3, platform_3_currentPalette, platform_3_targetPalette);
 
-  offset = 0;
-  segment_end = NUM_PLATFORM_1_LEDS * 3;
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Khaki;
-  }
-
-  offset = NUM_PLATFORM_1_LEDS * 3;
-  segment_end = offset + (NUM_PLATFORM_2_LEDS * 3);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Red;
-  }
-
-  offset = (NUM_PLATFORM_1_LEDS * 3) + (NUM_PLATFORM_2_LEDS * 3);
-  segment_end = offset + (NUM_PLATFORM_3_LEDS * 3);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Blue;
-  }
-
-  // platform 4
-  offset = NUM_LOWER_LEDS + (NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
-  segment_end = offset + (NUM_PLATFORM_4_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Green;
-  }
-  offset = NUM_LOWER_LEDS + (NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
-  segment_end = offset + (NUM_PLATFORM_4_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Green;
-  }
-  offset = NUM_LOWER_LEDS + (NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 2;
-  segment_end = offset + (NUM_PLATFORM_4_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Green;
-  }
-
-  // Spots
-  offset = NUM_LOWER_LEDS + (NUM_ANTENNA_LEDS);
-  segment_end = offset + (NUM_SPOT_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::White;
-  }
-  offset = NUM_LOWER_LEDS + (NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
-  segment_end = offset + (NUM_SPOT_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::White;
-  }
-  offset = NUM_LOWER_LEDS + (NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 2;
-  segment_end = offset + (NUM_SPOT_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::White;
-  }
-
-  // Antenna
-  offset = NUM_LOWER_LEDS;
-  segment_end = offset + (NUM_ANTENNA_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Green;
-  }
-  offset = NUM_LOWER_LEDS + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
-  segment_end = offset + (NUM_ANTENNA_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Green;
-  }
-  offset = NUM_LOWER_LEDS + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 2;
-  segment_end = offset + (NUM_ANTENNA_LEDS);
-  for (int i = offset; i < segment_end; i++)
-  {
-    leds[i] = CRGB::Green;
-  }
+  // for (int i = 0; i < NUM_PLATFORM_4_LEDS * 3; i++)
+  // {
+  //   leds[platform_4_leds[i]] = CRGB::DarkRed;
+  // }
+  // for (int i = 0; i < NUM_SPOT_LEDS * 3; i++)
+  // {
+  //   leds[spot_leds[i]] = CRGB::Purple;
+  // }
+  // for (int i = 0; i < NUM_ANTENNA_LEDS * 3; i++)
+  // {
+  //   leds[antenna_leds[i]] = CRGB::Orange;
+  // }
 }
 
 void loop()
@@ -140,6 +90,121 @@ void setupSerial()
   Serial.printf("Sketch compiled %s\n", compile_date);
 }
 
+void setupPlatform1LEDs()
+{
+  int index = 0;
+  int offset = 0;
+  int segment_end = NUM_PLATFORM_1_LEDS * 3;
+  for (int i = offset; i < segment_end; i++)
+  {
+    platform_1_leds[index] = i;
+    index++;
+  }
+}
+
+void setupPlatform2LEDS()
+{
+  int index = 0;
+  int offset = NUM_PLATFORM_1_LEDS * 3;
+  int segment_end = offset + (NUM_PLATFORM_2_LEDS * 3);
+  for (int i = offset; i < segment_end; i++)
+  {
+    platform_2_leds[index] = i;
+    index++;
+  }
+}
+
+void setupPlatform3LEDS()
+{
+  int index = 0;
+  int offset = (NUM_PLATFORM_1_LEDS * 3) + (NUM_PLATFORM_2_LEDS * 3);
+  int segment_end = offset + (NUM_PLATFORM_3_LEDS * 3);
+  for (int i = offset; i < segment_end; i++)
+  {
+    platform_3_leds[index] = i;
+    index++;
+  }
+}
+
+void setupPlatform4LEDS()
+{
+  int index = 0;
+  int offset = NUM_LOWER_LEDS + (NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
+  int segment_end = offset + (NUM_PLATFORM_4_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    platform_4_leds[index] = i;
+    index++;
+  }
+  offset = NUM_LOWER_LEDS + (NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
+  segment_end = offset + (NUM_PLATFORM_4_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    platform_4_leds[index] = i;
+    index++;
+  }
+  offset = NUM_LOWER_LEDS + (NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 2;
+  segment_end = offset + (NUM_PLATFORM_4_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    platform_4_leds[index] = i;
+    index++;
+  }
+}
+
+void setupSpotLEDS()
+{
+  int index = 0;
+  int offset = NUM_LOWER_LEDS + (NUM_ANTENNA_LEDS);
+  int segment_end = offset + (NUM_SPOT_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    spot_leds[index] = i;
+    index++;
+  }
+  offset = NUM_LOWER_LEDS + (NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
+  segment_end = offset + (NUM_SPOT_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    spot_leds[index] = i;
+    index++;
+  }
+  offset = NUM_LOWER_LEDS + (NUM_ANTENNA_LEDS) + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 2;
+  segment_end = offset + (NUM_SPOT_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    spot_leds[index] = i;
+    index++;
+  }
+}
+
+void setupAntennaLEDS()
+{
+  // Antenna
+  int index = 0;
+  int offset = NUM_LOWER_LEDS;
+  int segment_end = offset + (NUM_ANTENNA_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    antenna_leds[index] = i;
+    index++;
+  }
+  offset = NUM_LOWER_LEDS + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS);
+  segment_end = offset + (NUM_ANTENNA_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    antenna_leds[index] = i;
+    index++;
+  }
+  offset = NUM_LOWER_LEDS + (NUM_PLATFORM_4_LEDS + NUM_SPOT_LEDS + NUM_ANTENNA_LEDS) * 2;
+  segment_end = offset + (NUM_ANTENNA_LEDS);
+  for (int i = offset; i < segment_end; i++)
+  {
+    antenna_leds[index] = i;
+    index++;
+  }
+}
+
 void setupStrip()
 {
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
@@ -147,4 +212,11 @@ void setupStrip()
   FastLED.setBrightness(255);
   // Emperically this results in 50 watts from the wall
   set_max_power_in_milliwatts(57000);
+
+  setupPlatform1LEDs();
+  setupPlatform2LEDS();
+  setupPlatform3LEDS();
+  setupPlatform4LEDS();
+  setupSpotLEDS();
+  setupAntennaLEDS();
 }
---
+++ plasma.ino
@@ -0,0 +1,33 @@
+#define qsuba(x, b) ((x > b) ? x - b : 0)
+
+TBlendType currentBlending = LINEARBLEND;
+
+void plasma(int ledArray[], int max, CRGBPalette16 &currentPalette, CRGBPalette16 &targetPalette)
+{
+    int thisPhase = beatsin8(6, -64, 64);
+    int thatPhase = beatsin8(7, -64, 64);
+    Serial.print("Goint to max ");
+    Serial.println(max);
+    for (int k = 0; k < max; k++)
+    {
+        // For each of the LED's in the strand, set a brightness based on a wave as follows:
+        int colorIndex = cubicwave8((k * 23) + thisPhase) / 2 + cos8((k * 15) + thatPhase) / 2; // Create a wave and add a phase change and add another wave with its own phase change.. Hey, you can even change the frequencies if you wish.
+        int thisBright = qsuba(colorIndex, beatsin8(7, 0, 96));                                 // qsub gives it a bit of 'black' dead space by setting sets a minimum value. If colorIndex < current value of beatsin8(), then bright = 0. Otherwise, bright = colorIndex..
+        // Serial.print("Looking at index ");
+        // Serial.println(k);
+        // Serial.print("Which has led number:");
+
+        Serial.println(ledArray[k]);
+        leds[ledArray[k]] = ColorFromPalette(currentPalette, colorIndex, thisBright, currentBlending); // Let's now add the foreground colour.
+    }
+    EVERY_N_MILLISECONDS(100)
+    {
+        uint8_t maxChanges = 24;
+        nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges); // AWESOME palette blending capability.
+    }
+    EVERY_N_SECONDS(5)
+    {                              // Change the target palette to a random one every 5 seconds.
+        uint8_t baseC = random8(); // You can use this as a baseline colour if you want similar hues in the next line.
+        targetPalette = CRGBPalette16(CHSV(baseC + random8(32), 192, random8(128, 255)), CHSV(baseC + random8(32), 255, random8(128, 255)), CHSV(baseC + random8(32), 192, random8(128, 255)), CHSV(baseC + random8(32), 255, random8(128, 255)));
+    }
+}
