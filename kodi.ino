62afcc7d66bc0d000b33615edc2a284824c01bda
Kyle Anderson <kyle@xkyle.com>
2021-05-16 13:59:49 -0700

    Handle kodi timeouts

--- kodi.ino
+++ kodi.ino
@@ -10,19 +10,20 @@ bool isKodiPlaying(bool previous)
   http.begin(KODI_HOST, 8080, "/jsonrpc");
   http.addHeader("Content-Type", "application/json");
   uint16_t httpResponseCode;
-  
+
   httpResponseCode = http.POST("{\"jsonrpc\": \"2.0\",\"method\": \"Player.GetActivePlayers\",\"id\": 1}");
   if (httpResponseCode > 0) {
     String response = http.getString();
-    Serial.println(httpResponseCode);
-    Serial.println(response);
     http.end();
-    if (response == "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":[{\"playerid\":1,\"playertype\":\"internal\",\"type\":\"video\"}]}") {
+    if (httpResponseCode == 65535) {
+      return false;
+    } else if (response == "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":[{\"playerid\":1,\"playertype\":\"internal\",\"type\":\"video\"}]}") {
       return true;
     } else if ( response == "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":[]}") {
       return false;
     } else {
       Serial.println("Not sure what this response is from Kodi:");
+      Serial.println(httpResponseCode);
       Serial.println(response);
       return previous;
     }
